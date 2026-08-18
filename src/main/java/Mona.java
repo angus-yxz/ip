50ec6a9b456ccb18dc5486f467a026103834bf3c
angus-yxz <138585512+angus-yxz@users.noreply.github.com>
2026-08-19 05:34:35 +0800

    Add user input loop
    - Echo entered commands until the user enters bye, then display a farewell message.
    - Added utility function for formatted printing
    - Refactored code to extract banner and separator as constants

--- src/main/java/Mona.java
+++ src/main/java/Mona.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
+
 public class Mona {
+    private final static String separator = "____________________________________________________________";
+    private final static String banner = " __  __  ___  _   _    _ \n"
+            + "|  \\/  |/ _ \\| \\ | |  / \\\n"
+            + "| |\\/| | | | |  \\| | / _ \\\n"
+            + "| |  | | |_| | |\\  |/ ___ \\\n"
+            + "|_|  |_|\\___/|_| \\_/_/   \\_\\\n";
+
+    private static void printFormatted(String text) {
+        System.out.println(separator);
+        System.out.println(text);
+        System.out.println(separator);
+    }
+
     public static void main(String[] args) {
-        String banner = " __  __  ___  _   _    _ \n"
-                + "|  \\/  |/ _ \\| \\ | |  / \\\n"
-                + "| |\\/| | | | |  \\| | / _ \\\n"
-                + "| |  | | |_| | |\\  |/ ___ \\\n"
-                + "|_|  |_|\\___/|_| \\_/_/   \\_\\\n";
-        System.out.println("____________________________________________________________");
-        System.out.println(banner);
-        System.out.println("Hello! I'm Mona.\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        printFormatted(banner + "\n" + "Hello! I'm Mona.\nWhat can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        while (!exit) {
+            System.out.print("Mona > ");
+            String userInput = scanner.nextLine();
+            switch (userInput) {
+                case "bye":
+                    printFormatted("Bye. Hope to see you again soon!");
+                    exit = true;
+                    break;
+                default:
+                    printFormatted(userInput);
+            }
+        }
     }
 }
