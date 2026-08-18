0b2cceafbe9310bb3686b3fa775cf2c529b6d09b
angus-yxz <138585512+angus-yxz@users.noreply.github.com>
2026-08-19 05:19:44 +0800

    Renamed chatbot to Mona

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Mona User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Mona.java
@@ -0,0 +1,15 @@
+public class Mona {
+    public static void main(String[] args) {
+        String banner = " __  __  ___  _   _    _ \n"
+                + "|  \\/  |/ _ \\| \\ | |  / \\\n"
+                + "| |\\/| | | | |  \\| | / _ \\\n"
+                + "| |  | | |_| | |\\  |/ ___ \\\n"
+                + "|_|  |_|\\___/|_| \\_/_/   \\_\\\n";
+        System.out.println("____________________________________________________________");
+        System.out.println(banner);
+        System.out.println("Hello! I'm Mona.\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
